6772106b970ab99bc95115e2b2466682519bde44
RafaelEsaa <esaarafael@hotmail.com>
2018-11-05 10:25:49 -0400

    elimino comentarios

--- globals.php
+++ globals.php
@@ -1,6 +1,3 @@
 <?php
-
-// define('SITE_URL', 'https://bootcamp-comparison-alesanchezr.c9users.io');
-// define('API_URL', 'https://bootcamp-comparison-alesanchezr.c9users.io/api');
 define('SITE_URL', 'http://localhost/bootcamp-comparison/');
 define('API_URL', 'https://bootcamp-comparison-rafaelesaa.c9users.io/');
